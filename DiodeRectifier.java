581ad9c2f9e77e7043dbd576e1b192e4425afb52
Mahmudul Rapi <mrapi@princeton.edu>
2021-11-15 00:39:26 -0500

    Almost done!

--- DiodeRectifier.java
+++ DiodeRectifier.java
@@ -17,10 +17,11 @@ public class DiodeRectifier{
         double Vout = 0;
 
         if (waveform == 1) {
-            Vout = Vs / pi * (1 - w * L_c )
+            Vout = Vpk / (Math.PI + w * L_c / (2 * R));
         }
         else {
-            Vout = Vpk / 2;
+            double T = 2 * Math.PI / w;
+            Vout = Vpk / (2 * (1 + L_c / (R * T)));
         }
 
         if (isFullWave == 2) Vout *= 2; // double for full wave
@@ -58,7 +59,7 @@ public class DiodeRectifier{
         StdOut.println("Is the voltage waveform a sin wave or square? 1 for sin, 2 for square");
         input = StdIn.readInt();
         while (input != 1 && input != 2){
-            StdOut.println("Please enter 1 (Yes) or 2 (No)");
+            StdOut.println("Please enter 1 (sin) or 2 (square)");
             input = StdIn.readInt();
         }
         waveform = input;
@@ -83,6 +84,27 @@ public class DiodeRectifier{
                 }
             }
 
+        StdOut.println("What is frequency of the wave in Hz?");
+        double f = StdIn.readDouble();
+        w = f * 2 * Math.PI;
+        StdOut.printf("To confirm, frequency of %3.4f Hz (which is %2.4f rad/s or %2.4fs period)? 1 for Yes, 2 for No\n", f, w, 1 / f);
+        input = StdIn.readInt();
+        while (input != 1 && input != 2){
+            StdOut.println("Please enter 1 (Yes) or 2 (No) ");
+            input = StdIn.readInt();
+            }
+
+        while (input == 2){
+            StdOut.println("What is the peak voltage (amplitude of the wave) in volts");
+            w = StdIn.readDouble() * 2 * Math.PI;
+            StdOut.println("To confirm, peak voltage of " + Vpk + " volts? 1 for Yes, 2 for No");
+            input = StdIn.readInt();
+            while (input != 1 && input != 2){
+            StdOut.println("Please enter 1 (Yes) or 2 (No) ");
+            input = StdIn.readInt();
+            }
+        }
+             
         StdOut.println("Is there any parasitic inductance? 1 for Yes, 2 for No");
         input = StdIn.readInt();
         while (input != 1 && input != 2){
@@ -138,6 +160,9 @@ public class DiodeRectifier{
         else  
             paraComputeVout();
         
+        StdOut.printf("Output DC Voltage: %2.4f volts\n", Vo);
+        StdOut.printf("Output DC current: %2.4f volts\n", Io);
+
     }
      
 }
